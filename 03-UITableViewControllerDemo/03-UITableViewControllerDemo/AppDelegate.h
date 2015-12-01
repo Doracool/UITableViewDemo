72a8015380adf026b0463f7b7c7d7d1268de5f4f
Doracool <751379858@qq.com>
2015-12-01 20:18:45 +0800

    实现了单元格的复用

---
+++ 03-UITableViewControllerDemo/03-UITableViewControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  03-UITableViewControllerDemo
+//
+//  Created by qingyun on 15/12/1.
+//  Copyright (c) 2015年 河南青云信息技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 03-UITableViewControllerDemo/03-UITableViewControllerDemo/QYTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  QYTableViewController.h
+//  03-UITableViewControllerDemo
+//
+//  Created by qingyun on 15/12/1.
+//  Copyright (c) 2015年 河南青云信息技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QYTableViewController : UITableViewController
+
+@end
